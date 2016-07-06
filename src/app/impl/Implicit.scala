de7719ec50972094689860dc6e024fb1b3bcc20f
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-07-06 21:41:04 +0100

    bubble sort algorithm

---
+++ src/app/impl/Implicit.scala
@@ -0,0 +1,28 @@
+package app.impl
+
+import org.junit.Test
+
+
+class Implicit extends Generic[String, Long] {
+
+
+  /**
+    * Implicit parameter tell is used in case that there´s no parameters passed to the function,
+    * the compiler will take the one implicit val defined.
+    * if you define more than one implicit val the code wont compile.
+    */
+  @Test def implicitParameter(): Unit = {
+    println(multiply)
+    println(multiply(3, 3))
+  }
+
+  implicit val multiplier = 2
+
+  def multiply(implicit x: Int, y: Int) = {
+    x * y
+  }
+
+
+
+
+}
