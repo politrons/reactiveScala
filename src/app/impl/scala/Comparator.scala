6ef19fae5c4e63846e321675af797f453cf7df53
ZH11 <Pablo.Garcia@uk.tesco.com>
2016-10-02 22:15:58 +0100

    Sort

---
+++ src/app/impl/scala/Comparator.scala
@@ -0,0 +1,29 @@
+package app.impl.scala
+
+import org.junit.Test
+
+
+class Comparator {
+
+
+  /**
+    * Algorithm to sort array of numbers
+    */
+  @Test def sortNumbers(): Unit = {
+    val numbers = Array[Int](5, 3, 2, 6, 1, 4)
+    println(numbers.sorted.toList)
+  }
+
+  /**
+    * Algorithm to sort classes by attributes
+    */
+  @Test def sortObjects(): Unit = {
+    val men:List[Man] = List(new Man("pablo", 35), new Man("Nene", 35), new Man("Didu", 38), new Man("Bulum", 37))
+    println(men.sortBy(_.name))
+    println(men.sortBy(_.age))
+  }
+
+
+  class Man(val name: String, val age: Int) {}
+
+}
