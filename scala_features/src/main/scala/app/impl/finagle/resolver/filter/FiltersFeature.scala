537bf34b1fe9a1c4c30aaa71e16187f341b02cf4
politrons <politrons@gmail.com>
2018-04-23 15:50:23 +0200

    Add finagle filters

--- scala_features/src/main/scala/app/impl/finagle/resolver/filter/FiltersFeature.scala
+++ scala_features/src/main/scala/app/impl/finagle/resolver/filter/FiltersFeature.scala
@@ -5,6 +5,7 @@ import app.impl.finagle.workshop.FinagleHttpClient.response
 import com.twitter.finagle._
 import com.twitter.finagle.http.{Request, Response}
 import com.twitter.util.{Await, Future}
+import org.jboss.netty.handler.codec.http.HttpResponse
 import org.junit.Test
 
 class FiltersFeature {
@@ -19,26 +20,40 @@ class FiltersFeature {
   request.method(http.Method.Get)
   request.contentString = ""
 
-  val filter1 = new Filter[Request, Response, Request, Response] {
-    override def apply(request: Request, service: Service[Request, Response]): Future[Response] = {
-      val value = service.apply(request)
-      value
+  case class ResponseInfo(request: Request, response: Response) extends Response {
+    override protected def httpResponse: HttpResponse = null
+
+    override def toString =
+      "Response(response)"
+  }
+
+  val filter1 = new Filter[Request, ResponseInfo, Request, ResponseInfo] {
+    override def apply(request: Request, service: Service[Request, ResponseInfo]): Future[ResponseInfo] = {
+      service.apply(request)
+        .map(responseInfo => {
+          responseInfo.copy(response = responseInfo.response)
+        })
     }
   }
 
-  val filter2 = new Filter[Request, Response, Request, Response] {
-    override def apply(request: Request, service: Service[Request, Response]): Future[Response] = {
-      val value = service.apply(request)
-      value
+  val filter2 = new Filter[Request, ResponseInfo, Request, Response] {
+    override def apply(request: Request, service: Service[Request, Response]): Future[ResponseInfo] = {
+      service.apply(request)
+        .map(response => ResponseInfo(request, response))
     }
   }
 
   @Test
   def main(): Unit = {
-    val serviceRequest = filter1.andThen(filter2).andThen(service)
-    val response = serviceRequest(request)
-    println(Await.result(response))
 
+    val serviceRequest = filter1
+      .andThen(filter2)
+      .andThen(service)
+
+    val responseFuture = serviceRequest(request)
+    val response = Await.result(responseFuture)
+    println(response.asInstanceOf[ResponseInfo].request)
+    println(response.asInstanceOf[ResponseInfo].response.statusCode)
 
   }
 
