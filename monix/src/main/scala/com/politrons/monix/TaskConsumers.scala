79b1cfca1724d9a5a8a9dc5b56fcbe106e468a64
politrons <politrons@gmail.com>
2018-05-01 10:42:46 +0200

    Monix features

--- monix/src/main/scala/com/politrons/monix/TaskConsumers.scala
+++ monix/src/main/scala/com/politrons/monix/TaskConsumers.scala
@@ -1,5 +1,7 @@
 package com.politrons.monix
 
+import java.util.concurrent.TimeUnit
+
 import monix.eval.Task
 import monix.execution.Scheduler.Implicits.global
 import org.junit.Test
@@ -13,7 +15,7 @@ import scala.concurrent.duration._
   * The idea is simple. Separate consumer from producer. Here we can create some Producers called as [Task]
   * and that functions only it will be executed and consumed when someone use the specific operator
   */
-class TaskFeature {
+class TaskConsumers {
 
   //#####################
   //#     EXECUTORS     #
@@ -44,6 +46,27 @@ class TaskFeature {
   }
 
   /**
+    * A cancelable object it can be cancel at some point in time, if you want to cancel that process and clean the
+    * resource you just need to use cancel operator.
+    */
+  @Test
+  def cancelOnCompleteOperator(): Unit = {
+    val cancelable = Task("Hello Monix Task world!")
+      .map(value => value.toUpperCase)
+      .delayResult(FiniteDuration.apply(1, TimeUnit.SECONDS))
+      .runOnComplete {
+        case Success(value) =>
+          println(value)
+          value
+        case Failure(ex) =>
+          System.out.println(s"ERROR: ${ex.getMessage}")
+          ex
+      }
+    cancelable.cancel()
+    Thread.sleep(1000)
+  }
+
+  /**
     * In case we want to execute the logic of the task in another thread, and later to join the result of the execution
     * of the task in the main thread we use the operator [runAsync] which it will return a cancelableFuture which is
     * an extension of Scala future.
@@ -53,8 +76,42 @@ class TaskFeature {
     val cancelableFuture = Task("Hello Monix Task world!")
       .map(value => value.toUpperCase)
       .runAsync
-    cancelableFuture.foreach(println)
-    Await.result(cancelableFuture, 10 seconds)
+    val result = Await.result(cancelableFuture, 10 seconds)
+    println(result)
+  }
+
+  /**
+    * The coeval coeval is one of the most amazing operator I've ever seen. He calculate in runtime if a process
+    * is already done and we dont need to wait for him, or otherwise create a CancelableFuture to be run async.
+    * It return an Either[Throwable, Either[CancelableFuture[T], T ]]]
+    */
+  @Test
+  def coevalOperator(): Unit = {
+    val coeval = Task(getSentence(0.5))
+      .map(value => value.toUpperCase)
+      .coeval
+    coeval.run match {
+      case Right(either) => {
+        either match {
+          case Right(value) => println(s"Right value $value")
+          case Left(future) => println(s"We need to run this in a future ${Await.result(future, 10 second)}")
+        }
+      }
+      case Left(throwable) => println(s"An error just happen $throwable")
+    }
+  }
+
+  def getSentence(perc: Double): String = {
+    if (math.random < perc) {
+      throw new NullPointerException()
+    } else {
+      if (math.random < perc) {
+        Thread.sleep(1000)
+        "This test should  be executed async"
+      } else {
+        "Plain sync process"
+      }
+    }
   }
 
 }
