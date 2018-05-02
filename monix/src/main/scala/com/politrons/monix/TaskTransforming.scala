ca24ce0c4cebe38f6a6eb02e6e8962fd1c00e55d
politrons <politrons@gmail.com>
2018-05-02 10:49:52 +0200

    Monix observables

--- monix/src/main/scala/com/politrons/monix/TaskTransforming.scala
+++ monix/src/main/scala/com/politrons/monix/TaskTransforming.scala
@@ -10,11 +10,41 @@ import scala.concurrent.{Await, Future}
 
 class TaskTransforming {
 
+  /**
+    * In case of error we retry the whole emission until the specific number we pass to the operator
+    */
   @Test
-  def xxxx(): Unit = {
-
+  def onErrorRestartOperator(): Unit = {
+    val cancelableFuture = Task(getSentence(0.05))
+      .map(value => {
+        println(s"Process value: $value")
+        value.toUpperCase
+      })
+      .onErrorRestart(20)
+      .runAsync
+    val result = Await.result(cancelableFuture, 10 seconds)
+    println(result)
   }
 
+  /**
+    * Operator onErrorRestartIf only it will retry in case the Throwable passed is the same type as you specify
+    * in your predicate function.
+    */
+  @Test
+  def onErrorRestartIfOperator(): Unit = {
+    val cancelableFuture = Task(getSentence(0.05))
+      .map(value => {
+        println(s"Process value: $value")
+        value.toUpperCase()
+      })
+      .onErrorRestartIf(t => t.isInstanceOf[NullPointerException])
+      .runAsync
+    val result = Await.result(cancelableFuture, 10 seconds)
+    println(result)
+  }
 
+  def getSentence(perc: Double): String = {
+    if (math.random < perc) "Hi Task functional world" else null
+  }
 
 }
