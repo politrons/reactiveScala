d7a45c44d0f9f3bc9e9dc284233795295eebdffa
politrons <politrons@gmail.com>
2018-05-19 00:56:37 +0200

    Pure functional programing

--- category_theory/src/main/scala/types/ApplicativeFeature.scala
+++ category_theory/src/main/scala/types/ApplicativeFeature.scala
@@ -1,23 +1,19 @@
 package types
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
-
 object ApplicativeFeature extends App {
 
-
   /**
-    * Applicative type class has a constructor of Type T and is used when we have to compose two function at same time
-    * as it would happen in haskell since curried by default, where a function with two arguments like
-    * a => b => c actually func(a) => func((a) b) => c so here then we need to apply the very same function for both
-    * elements and then return a tuple with both values.
+    * Applicative type class has a constructor of Type T and is used when we have a function that receive two argumemnts
+    * as the function just process the first element it apply curried so it return a function so here what we have is that
+    * situation when I have the second argument F[A] to process and they pass me a function inside the constructor F[A => B]
     */
   trait ApplicativeType[F[_]] {
 
     def pure[A](a: A): F[A]
 
-    def product[A, B](a: F[A], b: F[A])(f: A => B): F[(B, B)]
+    def ap[A, B](fa: F[A])(f: F[A => B]): F[B]
+
+    def product[A, B, C](a: F[A], b: F[B])(func: A => B => C): F[C]
 
   }
 
@@ -27,44 +23,30 @@ object ApplicativeFeature extends App {
 
     override def pure[A](a: A): Option[A] = Some(a)
 
-    override def product[A, B](a: Option[A], b: Option[A])(f: A => B): Option[(B, B)] = {
-      a.flatMap(value => b.map(value1 => (f(value), f(value1))))
+    override def ap[A, B](input: Option[A])(f: Option[A => B]): Option[B] = {
+      input.flatMap(value => f.map(func => func(value)))
     }
 
+    override def product[A, B, C](a: Option[A], b: Option[B])(func: A => B => C): Option[C] = {
+      a.flatMap(aVal => b.map(bVal => {
+        val bToC = func(aVal)
+        bToC(bVal)
+      }))
+    }
   }
 
-  val oVal1 = applicativeOption.pure("hello")
-  val oVal2 = applicativeOption.pure(" world")
-  private val maybeTuple: Option[(String, String)] = applicativeOption.product(oVal1, oVal2)(x => x.toUpperCase)
+  val oVal1 = applicativeOption.pure("hello applicative world")
+  private val maybeTuple: Option[String] = applicativeOption.ap(oVal1)(applicativeOption.pure(x => x.toUpperCase))
   println(maybeTuple)
 
-  //  Future
-  //__________
-  private implicit val applicativeFuture = new ApplicativeType[Future] {
-
-    override def pure[A](a: A): Future[A] = Future(a)
-
-    override def product[A, B](a: Future[A], b: Future[A])(f: A => B): Future[(B, B)] = {
-      a.flatMap(value => b.map(value1 => (f(value), f(value1))))
-    }
-
-  }
-
-  private val fVaL1: Future[String] = applicativeFuture.pure("Hello")
-  private val fVaL2: Future[String] = applicativeFuture.pure(" World")
-  val future = applicativeFuture.product(fVaL1, fVaL2)(value => value.toUpperCase)
-  println(Await.result(future, 10 seconds))
 
   //    Type Class
   //  _____________
-  private val optionString = runType[String, String, Option](oVal1,oVal2, a => s"$a applicative Option type class")
+  private val optionString = runType[String, String, Option](oVal1, Option(a => s"$a applicative Option type class"))
   println(optionString)
 
-  private val futureString = runType[String, String, Future](fVaL1,fVaL2, a => s"$a applicative future type class")
-  println(Await.result(futureString, 10 seconds))
-
-  def runType[A, B, F[_]](a: F[A], b: F[A], f: A => B)(implicit applicativeType: ApplicativeType[F]): F[(B,B)] = {
-    applicativeType.product(a, b)(f)
+  def runType[A, B, F[_]](a: F[A], f: F[A => B])(implicit applicativeType: ApplicativeType[F]): F[B] = {
+    applicativeType.ap(a)(f)
   }
 
 
---
+++ category_theory/src/main/scala/types/MonoidalFeature.scala
@@ -0,0 +1,71 @@
+package types
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.concurrent.{Await, Future}
+
+object MonoidalFeature extends App {
+
+
+  /**
+    * MonoidalType type class has a constructor of Type T and is used when we have to compose two function at same time
+    * as it would happen in haskell since curried by default, where a function with two arguments like
+    * a => b => c actually func(a) => func((a) b) => c so here then we need to apply the very same function for both
+    * elements and then return a tuple with both values.
+    */
+  trait MonoidalType[F[_]] {
+
+    def pure[A](a: A): F[A]
+
+    def apply[A, B](a: F[A], b: F[A])(f: A => B): F[(B, B)]
+
+  }
+
+  //  Option
+  //__________
+  private implicit val monoidalOption = new MonoidalType[Option] {
+
+    override def pure[A](a: A): Option[A] = Some(a)
+
+    override def apply[A, B](a: Option[A], b: Option[A])(f: A => B): Option[(B, B)] = {
+      a.flatMap(value => b.map(value1 => (f(value), f(value1))))
+    }
+
+  }
+
+  val oVal1 = monoidalOption.pure("hello")
+  val oVal2 = monoidalOption.pure(" world")
+  private val maybeTuple: Option[(String, String)] = monoidalOption.apply(oVal1, oVal2)(x => x.toUpperCase)
+  println(maybeTuple)
+
+  //  Future
+  //__________
+  private implicit val monoidalFuture = new MonoidalType[Future] {
+
+    override def pure[A](a: A): Future[A] = Future(a)
+
+    override def apply[A, B](a: Future[A], b: Future[A])(f: A => B): Future[(B, B)] = {
+      a.flatMap(value => b.map(value1 => (f(value), f(value1))))
+    }
+
+  }
+
+  private val fVaL1: Future[String] = monoidalFuture.pure("Hello")
+  private val fVaL2: Future[String] = monoidalFuture.pure(" World")
+  val future = monoidalFuture.apply(fVaL1, fVaL2)(value => value.toUpperCase)
+  println(Await.result(future, 10 seconds))
+
+  //    Type Class
+  //  _____________
+  private val optionString = runType[String, String, Option](oVal1,oVal2, a => s"$a applicative Option type class")
+  println(optionString)
+
+  private val futureString = runType[String, String, Future](fVaL1,fVaL2, a => s"$a applicative future type class")
+  println(Await.result(futureString, 10 seconds))
+
+  def runType[A, B, F[_]](a: F[A], b: F[A], f: A => B)(implicit applicativeType: MonoidalType[F]): F[(B,B)] = {
+    applicativeType.apply(a, b)(f)
+  }
+
+
+}
