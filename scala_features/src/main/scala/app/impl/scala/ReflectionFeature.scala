6b981ca13a4727f7fe0cb86e2ca8a4dee0f4e670
politrons <politrons@gmail.com>
2018-06-25 17:47:56 +0200

    Algorithm

--- scala_features/src/main/scala/app/impl/scala/ReflectionFeature.scala
+++ scala_features/src/main/scala/app/impl/scala/ReflectionFeature.scala
@@ -6,7 +6,7 @@ import org.junit.Test
   * Universe is the entry point to Scala reflection. A universe provides an interface to all the principal concepts
   * used in reflection, such as Types, Trees, and Annotations
   */
-import scala.reflect.runtime.{universe => ru}
+import scala.reflect.runtime.{universe}
 
 class ReflectionFeature {
 
@@ -20,7 +20,7 @@ class ReflectionFeature {
   def inspecting(): Unit = {
     val l = List(1, 2, 3)
     // using  ru.typeTag[T] we create a TypeTag of T
-    def getTypeTag[T: ru.TypeTag](obj: T) = ru.typeTag[T]
+    def getTypeTag[T: universe.TypeTag](obj: T) = universe.typeTag[T]
     //One of the API is obtain the type of the class
     val theType = getTypeTag(l).tpe
     println(theType)
@@ -30,33 +30,49 @@ class ReflectionFeature {
   }
 
   //  INSTANTIATING A TYPE AT RUNTIME
+  /**
+    * All the access to attributes of a class is always done through the [ClassMirror].
+    * Previously this class mirror is obtain through a Mirror that represent all ClassLoader together
+    * with the TypeTag of the class [Person]
+    * Once you have the ClassMirror you can obtain the declared methods of the ClassMirror such as the constructor.
+    * Then invoking the constructor you're able to create a new instance of Person
+    */
   @Test
   def instantiating(): Unit = {
     //  In the first step we obtain a mirror m which makes all classes and
     //  types available that are loaded by the current classloader, including class Person.
-    val mirror = ru.runtimeMirror(getClass.getClassLoader)
-    val classPerson = ru.typeOf[Person].typeSymbol.asClass
+    val mirror = universe.runtimeMirror(getClass.getClassLoader)
+    val classPerson = universe.typeOf[Person].typeSymbol.asClass
     //We create a person Mirror as representation of the Person class
     val personMirror = mirror.reflectClass(classPerson)
     //We create a constructor Symbol, which is used together with a mirror to obtain the element
-    val personConstructorSymbol = ru.typeOf[Person].decl(ru.termNames.CONSTRUCTOR).asMethod
+    val personConstructorSymbol = universe.typeOf[Person].decl(universe.termNames.CONSTRUCTOR).asMethod
     val personConstructor = personMirror.reflectConstructor(personConstructorSymbol)
     val person = personConstructor("Mike")
     println(person)
   }
 
   //  ACCESSING AND INVOKING MEMBERS OF RUNTIME TYPES
+  /**
+    * All the access to attributes of an instance is always done through the [InstanceMirror].
+    * In this case if you have a instance, is much easier to access one of the maybe private access attributes of a class.
+    * You just need to obtain a mirror, which you can just use the own instance person to get the classLoader and then
+    * Through the universe obtain the mention mirror.
+    * Before use that mirror we need a representation of the field we want to access. That is accessible through the TermSymbol
+    * Once we have the mirror and the termSymbol, using [reflect] passing the instance we obtain the [InstanceMirror] and with this
+    * mirror using reflectField and passing the symbol we obtain the value of the attribute.
+    */
   @Test
   def accessing(): Unit = {
     val person = Purchase("Paul Perez", 28232, false)
     //As before we create the mirror using the the classLoader of person class
-    val mirror = ru.runtimeMirror(person.getClass.getClassLoader)
+    val mirror = universe.runtimeMirror(person.getClass.getClassLoader)
     //We get the TermSymbol of the attribute shipped by name.
-    val shippingTermSymb = ru.typeOf[Purchase].decl(ru.TermName("shipped")).asTerm
+    val shippingTermSymbol = universe.typeOf[Purchase].decl(universe.TermName("shipped")).asTerm
    //We create an instance Mirror which we will use to extract together with the Symbol the value of attribute.
     val instanceMirror = mirror.reflect(person)
     //A field mirror which already contains the access of the value for the attribute we're looking for.
-    val shippingFieldMirror = instanceMirror.reflectField(shippingTermSymb)
+    val shippingFieldMirror = instanceMirror.reflectField(shippingTermSymbol)
     println(shippingFieldMirror.get)
 
   }
