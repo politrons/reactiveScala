390ed479af3864fd4e26fe5ffd1cd04d2ac29bf7
politrons <politrons@gmail.com>
2018-12-15 12:31:12 +0100

    Add Fs2 Stream

--- fs2/src/main/scala/StreamFeature.scala
+++ fs2/src/main/scala/StreamFeature.scala
@@ -1,10 +1,7 @@
+import cats.effect.IO
 import fs2.Stream
 import org.junit.Test
-import cats.effect.IO
-
-import scala.reflect.internal.Chars
-import scala.util.Random
-
+import cats.effect.ContextShift
 
 /**
   * F2s Stream it was an initiative of ScalaZ community to create a reactive Stream for scala.
@@ -15,26 +12,34 @@ import scala.util.Random
   */
 class StreamFeature {
 
-  /**-------------------*/
-  /**     CREATION      */
-  /**-------------------*/
+  /** ------------------- */
+  /** CREATION      */
+  /** ------------------- */
   /**
     * To initialize the Stream, we can use operator:
     * * [empty] in case we dont to emit anything
     * * [emit] to start the emission in eager way and create the pipeline with the value right away.
+    * * [emits] a Seq of elements in the pipeline.
     * * [eval] to create a lazy value which it will evaluated and become impure once we compile and use [unsafeRunSync]
+    * * [range] to create a stream that emit from A to B range
+    * * [raiseError] create an Stream that emit an exception.
     */
   @Test
   def mainCreateStream: Unit = {
     println(Stream.empty)
     println(Stream.emit("hello", "Fs2", "stream", "world"))
-    println(Stream.emit(List("hello", "Fs2", "stream", "world")))
-    println(Stream.eval(IO{List("hello", "Fs2", "Pure", "world")}))
+    println(Stream.emits(List("hello", "Fs2", "stream", "world")))
+    println(Stream.eval(IO {
+      List("hello", "Fs2", "Pure", "world")
+    }))
+    println(Stream.range(1, 10))
+    println(Stream.raiseError[IO](new Exception("Controlled exception"))
+    )
   }
 
-  /**-------------------*/
-  /**     COMPOSITION   */
-  /**-------------------*/
+  /** ------------------- */
+  /** COMPOSITION   */
+  /** ------------------- */
   /**
     * Just to pass from Stream to elements you can use [toList] and [toVector] between others.
     * Also [Stream] as a monad allow the use of [map] for transformation, and [flatMap] for composition
@@ -61,12 +66,36 @@ class StreamFeature {
       composedTuples <- Stream.emit(tuples, 6, 7, 8, 9, 10)
     } yield composedTuples
     println(composedTuplesStream.toList)
-
   }
 
-  /**-------------------*/
-  /**     EFFECTS       */
-  /**-------------------*/
+  implicit val ioContextShift: ContextShift[IO] = IO.contextShift(scala.concurrent.ExecutionContext.Implicits.global)
+
+  /**
+    * [Merge] operator allow parallelism works when we are emitting elements, so in this case here we emmit elements
+    * from the first collection but we merge with the other two Streams running async, once they finish.
+    * In order to make the merge operator work in another thread, just like in Scala future we need to provide an implicit [ExecutionContext]
+    */
+  @Test
+  def mergeStreams: Unit = {
+    val mergedValues = Stream("hello", "async")
+      .merge(Stream.eval(IO {
+        println(s"Process in Thread:${Thread.currentThread().getName}")
+        Thread.sleep(500)
+        "Fs2"
+      }))
+      .merge(Stream.eval(IO {
+        println(s"Process in Thread:${Thread.currentThread().getName}")
+        Thread.sleep(500)
+        "World"
+      }))
+      .map(value => value.toUpperCase)
+      .compile.toVector.unsafeRunSync()
+    println(mergedValues)
+  }
+
+  /** ------------------- */
+  /** EFFECTS       */
+  /** ------------------- */
   /**
     * [Eval] operator creates a single element stream that gets its value by evaluating the supplied effect.
     * For the effect we use IO monad, similar to Haskell IO, it wrap the emission in this monad and control the effects.
@@ -119,9 +148,9 @@ class StreamFeature {
     println(emission)
   }
 
-  /**------------------------*/
-  /**     ERROR HANDLING     */
-  /**------------------------*/
+  /** ------------------------ */
+  /** ERROR HANDLING     */
+  /** ------------------------ */
   /**
     * Error handling [handleErrorWith] operator, just work as Scala future [recovery] or ReactiveX [onErrorResponse]
     * They catch the error throw in the pipeline and allow you to give you the chance to control the effect
@@ -159,9 +188,66 @@ class StreamFeature {
     print(err)
   }
 
-  /**------------------------*/
-  /**       RESOURCES        */
-  /**------------------------*/
+  /** ---------- */
+  /** RESOURCES **/
+  /** ---------- */
 
+  /**
+    * [Bracket] operator creates a stream that emits a resource allocated by an effect, ensuring the resource is
+    * eventually released regardless of how the stream is used.
+    * Bracket use curly to create a function that receive a monad (acquire:F[R]) and then the (release: R => F[Unit]) function
+    * which is invoked once the result emitted by [acquire] Stream terminates.
+    */
+  @Test
+  def controlResource: Unit = {
+    val count = new java.util.concurrent.atomic.AtomicLong(0)
+    val acquire = IO {
+      val incrementNum = count.incrementAndGet
+      println("Before acquire resource: " + incrementNum)
+      incrementNum
+    }
+    val release = IO {
+      println("After release resource: " + count.decrementAndGet);
+      ()
+    }
+    Stream.bracket(acquire)(_ => release)
+      .flatMap(num => Stream(100).map(num1 => num + num1))
+      .map(value => {
+        println(s"Number processed:$value")
+        value
+      })
+      .compile.drain.unsafeRunSync()
+  }
+
+  /** ---------------- **/
+  /** UTILS OPERATORS  **/
+  /** ---------------- **/
+
+  /**
+    * Just like other Streams solutions Fs2 keep most of the most handy operators to interact with.
+    * * [take] operator take first N elements from the emission of the pipeline.
+    * * [drop] operator drop first N elements from the emission of the pipeline.
+    * * [takeWhile] operator it will take elements emitted while the predicate function apply
+    * * [scan] behave just like left fold, we give an initial value and then pass a tuple to a function with the new emitted
+    * value, and also the previous iteration value.
+    */
+  @Test
+  def utilsOperators: Unit = {
+    val takeList = Stream.emits(List("hello", "Fs2", "stream", "world"))
+      .take(2)
+      .toList
+    println(takeList)
+    val droppedList = Stream.emits(List("hello", "Fs2", "stream", "world"))
+      .drop(2)
+      .toList
+    println(droppedList)
+    val takeWhileList = Stream.emits(List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
+      .takeWhile(num => num <= 5)
+      .toList
+    println(takeWhileList)
+    val scanList = Stream.range(1, 10).scan(0)((oldValue, newValue) => oldValue + newValue).toList
+    println(scanList)
+
+  }
 
 }
