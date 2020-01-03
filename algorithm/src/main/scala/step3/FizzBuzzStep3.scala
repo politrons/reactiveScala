f802fc953cb584303972c7a2b8beaee82148fc99
nb38tv <pablo.perez.garcia@ing.com>
2020-01-03 20:40:46 +0100

    Algorithm

--- algorithm/src/main/scala/step3/FizzBuzzStep3.scala
+++ algorithm/src/main/scala/step3/FizzBuzzStep3.scala
@@ -2,22 +2,24 @@ package step3
 
 object FizzBuzzStep3 {
 
+  /**
+    * Function to update the map counter of types.
+    */
+  private lazy val increaseCounterFunc: (Map[String, Int], String) => Map[String, Int] =
+    (elements, key) => elements.updated(key, elements.getOrElse(key, 0) + 1)
+
   def apply(_from: Int, _to: Int): Map[String, Int] = {
     (_from to _to)
       .foldLeft(Map[String, Int]())((elements, number) => {
         number match {
-          case num if num.toString.toCharArray.contains(51 /*Ascii code*/) =>
-            elements.updated("lucky", elements.getOrElse("lucky", 0) + 1)
-          case num if num % 15 == 0 =>
-            elements.updated("fizzbuzz", elements.getOrElse("fizzbuzz", 0) + 1)
-          case num if num % 5 == 0 =>
-            elements.updated("buzz", elements.getOrElse("buzz", 0) + 1)
-          case num if num % 3 == 0 =>
-            elements.updated("fizz", elements.getOrElse("fizz", 0) + 1)
-          case _ =>
-            elements.updated("integer", elements.getOrElse("integer", 0) + 1)
+          case num if num.toString.toCharArray.contains(51) => increaseCounterFunc(elements, "lucky")
+          case num if num % 15 == 0 => increaseCounterFunc(elements, "fizzbuzz")
+          case num if num % 5 == 0 => increaseCounterFunc(elements, "buzz")
+          case num if num % 3 == 0 => increaseCounterFunc(elements, "fizz")
+          case _ => increaseCounterFunc(elements, "integer")
         }
       })
   }
+
 
 }
