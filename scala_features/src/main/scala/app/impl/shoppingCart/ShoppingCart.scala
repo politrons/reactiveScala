e2361d2d7f1955630ac53e6974d898795cf28920
nb38tv <Madrid05>
2020-01-26 00:05:52 +0100

    basket

--- scala_features/src/main/scala/app/impl/shoppingCart/ShoppingCart.scala
+++ scala_features/src/main/scala/app/impl/shoppingCart/ShoppingCart.scala
@@ -3,16 +3,25 @@ package app.impl.shoppingCart
 case class ShoppingCart() {
 
   var basket: Basket = Basket(List())
+  val AppleType = "apple"
+  val OrangeType = "orange"
 
-  //Domain
-  val Apple = "apple"
-  val Orange = "orange"
+  //  ADT
+  // ------
+  sealed trait DiscountProduct
+
+  case class Apple(amount: Int) extends DiscountProduct
+
+  case class Orange(amount: Int) extends DiscountProduct
 
   case class Product(productId: String, description: String, price: BigDecimal)
 
   case class Basket(products: List[Product])
 
-  case class CheckoutInfo(totalPrice: BigDecimal, apples: Int, oranges: Int)
+  case class CheckoutInfo(totalPrice: BigDecimal, apples: Apple, oranges: Orange)
+
+  // Logic
+  // -------
 
   def addProduct(productId: String, description: String, price: BigDecimal): List[Product] = {
     basket = basket.copy(products = basket.products ++ List(Product(productId, description, price)))
@@ -21,24 +30,24 @@ case class ShoppingCart() {
 
   def checkout(): BigDecimal = {
     val checkoutInfo = getCheckoutInfo
-    applyDiscount(Orange, applyDiscount(Apple, checkoutInfo.totalPrice, checkoutInfo.apples), checkoutInfo.oranges)
+    applyDiscount(applyDiscount(checkoutInfo.totalPrice, checkoutInfo.apples), checkoutInfo.oranges)
   }
 
   private def getCheckoutInfo: CheckoutInfo = {
-    basket.products.foldRight(CheckoutInfo(BigDecimal(0.0), 0, 0))((product, checkout) => {
+    basket.products.foldRight(CheckoutInfo(BigDecimal(0.0), Apple(0), Orange(0)))((product, checkout) => {
       product.description match {
-        case Apple => checkout.copy(product.price + checkout.totalPrice, checkout.apples + 1, checkout.oranges)
-        case Orange => checkout.copy(product.price + checkout.totalPrice, checkout.apples, checkout.oranges + 1)
+        case AppleType => checkout.copy(product.price + checkout.totalPrice, Apple(checkout.apples.amount + 1), checkout.oranges)
+        case OrangeType => checkout.copy(product.price + checkout.totalPrice, checkout.apples, Orange(checkout.oranges.amount + 1))
         case _ => checkout.copy(product.price + checkout.totalPrice)
       }
     })
   }
 
-  private def applyDiscount: (String, BigDecimal, Int) => BigDecimal = {
-    (product, price, productNumber) =>
-      product match {
-        case _ if product == Apple && productNumber > 1 => price - 0.6 * (productNumber / 2)
-        case _ if product == Orange && productNumber > 1 => price - 0.25 * (productNumber / 3)
+  private def applyDiscount: (BigDecimal, DiscountProduct) => BigDecimal = {
+    (price, discountProduct) =>
+      discountProduct match {
+        case apple: Apple if apple.amount > 1 => price - 0.6 * (apple.amount / 2)
+        case orange: Orange if orange.amount > 1 => price - 0.25 * (orange.amount / 3)
         case _ => price
       }
   }
