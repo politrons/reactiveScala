5e84d6cb277208fdf91a3bddd8681103e1273d1c
politrons <politrons@gmail.com>
2020-03-30 19:20:27 +0200

    Http hedged client

--- scala_features/src/main/scala/app/impl/http/HedgedClientRunner.scala
+++ scala_features/src/main/scala/app/impl/http/HedgedClientRunner.scala
@@ -1,23 +1,47 @@
 package app.impl.http
 
 import com.twitter.util.{Await, Future}
+import org.junit.Test
 import zio.{Has, ZIO}
 
-object HedgedClientRunner extends App {
+/**
+ * Here we create our programs using the DSL of HttpHedgedClient, and using the different provide
+ * like finagle
+ */
+class HedgedClientRunner {
 
-  private val httpGetProgram: ZIO[Has[HttpHedgedClient.Service], Nothing, Future[Any]] = for {
-    _ <- HttpHedgedClient.Get()
-    _ <- HttpHedgedClient.Uri("/")
-    _ <- HttpHedgedClient.Host("www.google.com:80")
-    _ <- HttpHedgedClient.Timeout(5000)
-    _ <- HttpHedgedClient.Hedged(10)
-    future <- HttpHedgedClient.Run()
-  } yield future
+  @Test
+  def withSugarSyntax(): Unit = {
+    val httpGetProgram: ZIO[Has[HttpHedgedClient.Service], Nothing, Future[Any]] =
+      for {
+        _ <- HttpHedgedClient.Get()
+        _ <- HttpHedgedClient.Uri("/")
+        _ <- HttpHedgedClient.Host("www.google.com:80")
+        _ <- HttpHedgedClient.Timeout(5000)
+        _ <- HttpHedgedClient.Hedged(10)
+        future <- HttpHedgedClient.Run()
+      } yield future
 
-  private val programResponse: Future[Any] = HttpHedgedClient.runtime.unsafeRun {
-    httpGetProgram.provideCustomLayer(HttpHedgedClient.finagleEngine)
+    val programResponse: Future[Any] = HttpHedgedClient.runtime.unsafeRun {
+      httpGetProgram.provideCustomLayer(HttpHedgedClient.finagleEngine)
+    }
+    println(Await.ready(programResponse))
   }
 
-  println(Await.ready(programResponse))
+  @Test
+  def noSugarSyntax(): Unit = {
+    val httpGetProgram: ZIO[Has[HttpHedgedClient.Service], Nothing, Future[Any]] =
+      HttpHedgedClient.Get()
+        .flatMap(_ => HttpHedgedClient.Uri("/"))
+        .flatMap(_ => HttpHedgedClient.Host("www.google.com:80"))
+        .flatMap(_ => HttpHedgedClient.Timeout(5000))
+        .flatMap(_ => HttpHedgedClient.Hedged(10))
+        .flatMap(_ => HttpHedgedClient.Run())
+
+    val programResponse: Future[Any] = HttpHedgedClient.runtime.unsafeRun {
+      httpGetProgram.provideCustomLayer(HttpHedgedClient.finagleEngine)
+    }
+    println(Await.ready(programResponse))
+  }
 
 }
