00fac49ba3a94a78ba5671d4f3acd777f92471b5
politrons <politrons@gmail.com>
2020-08-28 00:57:25 +0200

    add gRPC

--- zio/src/main/scala/com/features/grpc/ZIOgRPC.scala
+++ zio/src/main/scala/com/features/grpc/ZIOgRPC.scala
@@ -1,35 +1,48 @@
 package com.features.grpc
 
-import com.features.zio.helloworld.{ConnectorManagerGrpc, RequestInfo, ResponseInfo}
+import com.features.zio.connectorManager.{ConnectorInfoDTO, ConnectorManagerGrpc, ResponseInfo}
 import io.grpc.{ManagedChannel, ManagedChannelBuilder, ServerBuilder}
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{Await, ExecutionContext, Future}
+import scala.concurrent.duration._
 
 object ZIOgRPC extends App {
 
-  val server = ServerBuilder.forPort(9999)
-    .addService(ConnectorManagerGrpc.bindService(new ConnectorManagerImpl, ExecutionContext.global)).asInstanceOf[ServerBuilder[_]].build
-  server.start
+  gRPCServer
+  makeRequest()
 
-  val channel: ManagedChannel = ManagedChannelBuilder.forAddress("localhost", 9999)
-    .usePlaintext()
-    .asInstanceOf[ManagedChannelBuilder[_]]
-    .build()
+  private def makeRequest() = {
+    val connectorManagerStub = createConnectorManager()
+    val request = ConnectorInfoDTO(connectorName = "Rest", requestInfo = "READ")
+    val reply: Future[ResponseInfo] = connectorManagerStub.connectorRequest(request)
+    println(Await.result(reply, 10 seconds).message)
+  }
 
-  val request = RequestInfo(name = "World")
+  private def createConnectorManager():ConnectorManagerGrpc.ConnectorManagerStub = {
+    val channel: ManagedChannel = ManagedChannelBuilder.forAddress("localhost", 9999)
+      .usePlaintext()
+      .asInstanceOf[ManagedChannelBuilder[_]]
+      .build()
+   ConnectorManagerGrpc.stub(channel)
+  }
 
-  val blockingStub = ConnectorManagerGrpc.blockingStub(channel)
-  val reply: ResponseInfo = blockingStub.connectorRequest(request)
-  println(reply.message)
+  private def gRPCServer = {
+    val service = ConnectorManagerGrpc.bindService(new ConnectorManagerImpl, ExecutionContext.global)
+    val server = ServerBuilder.forPort(9999)
+      .addService(service)
+      .asInstanceOf[ServerBuilder[_]]
+      .build
+    server.start
+  }
 
+  private class ConnectorManagerImpl extends ConnectorManagerGrpc.ConnectorManager {
+    override def connectorRequest(connector: ConnectorInfoDTO): Future[ResponseInfo] = {
+      val reply = ResponseInfo(message = s"Some logic ${connector.requestInfo} of ${connector.connectorName}")
+      Future.successful(reply)
+    }
 
-}
-
-
-private class ConnectorManagerImpl extends ConnectorManagerGrpc.ConnectorManager {
-  override def connectorRequest(req: RequestInfo): Future[ResponseInfo] = {
-    val reply = ResponseInfo(message = s"Hello ${req.name}")
-    Future.successful(reply)
   }
 
 }
+
+
