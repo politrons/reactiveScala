73f33a27df4e7dc06b70ca1f88fac2df46906154
politrons <politrons@gmail.com>
2020-08-28 00:46:01 +0200

    add gRPC

--- zio/src/main/scala/com/features/grpc/ZIOgRPC.scala
+++ zio/src/main/scala/com/features/grpc/ZIOgRPC.scala
@@ -1,16 +1,14 @@
 package com.features.grpc
 
-import com.features.zio.helloworld.{GreeterGrpc, HelloReply, HelloRequest}
-import io.grpc.{ManagedChannel, ManagedChannelBuilder, Server, ServerBuilder, ServerServiceDefinition}
-import io.grpc.Server
-import io.grpc.ServerBuilder
+import com.features.zio.helloworld.{ConnectorManagerGrpc, RequestInfo, ResponseInfo}
+import io.grpc.{ManagedChannel, ManagedChannelBuilder, ServerBuilder}
 
 import scala.concurrent.{ExecutionContext, Future}
 
 object ZIOgRPC extends App {
 
   val server = ServerBuilder.forPort(9999)
-    .addService(GreeterGrpc.bindService(new GreeterImpl, ExecutionContext.global)).asInstanceOf[ServerBuilder[_]].build
+    .addService(ConnectorManagerGrpc.bindService(new ConnectorManagerImpl, ExecutionContext.global)).asInstanceOf[ServerBuilder[_]].build
   server.start
 
   val channel: ManagedChannel = ManagedChannelBuilder.forAddress("localhost", 9999)
@@ -18,19 +16,19 @@ object ZIOgRPC extends App {
     .asInstanceOf[ManagedChannelBuilder[_]]
     .build()
 
-  val request = HelloRequest(name = "World")
+  val request = RequestInfo(name = "World")
 
-  val blockingStub = GreeterGrpc.blockingStub(channel)
-  val reply: HelloReply = blockingStub.sayHello(request)
-  println(reply)
+  val blockingStub = ConnectorManagerGrpc.blockingStub(channel)
+  val reply: ResponseInfo = blockingStub.connectorRequest(request)
+  println(reply.message)
 
 
 }
 
 
-private class GreeterImpl extends GreeterGrpc.Greeter {
-  override def sayHello(req: HelloRequest): Future[HelloReply] = {
-    val reply = HelloReply(message = "Hello " + req.name)
+private class ConnectorManagerImpl extends ConnectorManagerGrpc.ConnectorManager {
+  override def connectorRequest(req: RequestInfo): Future[ResponseInfo] = {
+    val reply = ResponseInfo(message = s"Hello ${req.name}")
     Future.successful(reply)
   }
 
