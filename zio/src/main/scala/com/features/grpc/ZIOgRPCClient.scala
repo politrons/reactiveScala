e82c08141b9a3cdcabe067c1f27482c0ab535947
politrons <politrons@gmail.com>
2020-08-28 10:38:10 +0200

    add gRPC

--- zio/src/main/scala/com/features/grpc/ZIOgRPCClient.scala
+++ zio/src/main/scala/com/features/grpc/ZIOgRPCClient.scala
@@ -1,26 +1,38 @@
 package com.features.grpc
 
-import com.features.zio.connectorManager.{ConnectorInfoDTO, ConnectorManagerGrpc, ResponseInfo}
+import com.features.zio.connectorManager.{ConnectorInfoDTO, ConnectorManagerGrpc}
 import io.grpc.{ManagedChannel, ManagedChannelBuilder}
-
-import scala.concurrent.duration._
-import scala.concurrent.{Await, Future}
+import zio.{Task, ZIO}
+import zio.Runtime.{default => Main}
 
 object ZIOgRPCClient extends App {
 
-  val connectorManagerStub = ConnectorManagerGrpc.stub(createChannel)
-  val request = ConnectorInfoDTO(connectorName = "Rest", requestInfo = "READ")
-  val reply: Future[ResponseInfo] = connectorManagerStub.connectorRequest(request)
-  println(Await.result(reply, 10 seconds).message)
+  private val clientProgram: ZIO[Any, Throwable, Unit] = (for {
+    channel <- createChannel()
+    connectorManagerStub <- ZIO.effect(ConnectorManagerGrpc.stub(channel))
+    request <- ZIO.effect(ConnectorInfoDTO(connectorName = "Rest", requestInfo = "READ"))
+    response <- ZIO.fromFuture(_ => connectorManagerStub.connectorRequest(request))
+    _ <- ZIO.succeed(println(s"Response: ${response.message}"))
+  } yield ()).catchAll(t => {
+    println(s"Error running ZIO gRPC client. Caused by $t")
+    ZIO.fail(t)
+  })
+  Main.unsafeRun(clientProgram)
 
-  private def createChannel = {
-    val channel: ManagedChannel = ManagedChannelBuilder.forAddress("localhost", 9999)
-      .usePlaintext()
-      .asInstanceOf[ManagedChannelBuilder[_]]
-      .build()
-    channel
+  private def createChannel(): Task[ManagedChannel] = {
+    ZIO.effect {
+      ManagedChannelBuilder.forAddress("localhost", 9999)
+        .usePlaintext()
+        .asInstanceOf[ManagedChannelBuilder[_]]
+        .build()
+    }
   }
 
 }
+
+/*
+java -cp zio-1.0-SNAPSHOT-jar-with-dependencies.jar com.features.grpc.ZIOgRPCServer
+*/
+
 
 
